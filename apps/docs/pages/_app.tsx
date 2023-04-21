d34bc8b532ff376a572580fc627456ac22419aa6
Bowen7 <zwxdyx@foxmail.com>
2023-04-21 19:16:12 +0800

    Add @vercel/analytics

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
